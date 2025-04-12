5b059341ed7ecb79a4554314eb31757f4ca55f3f
Suki <tues1205@naver.com>
2025-04-12 23:25:34 +0900

    💄: font 적용

--- Week4/seohui/src/styles/globalStyles.js
+++ Week4/seohui/src/styles/globalStyles.js
@@ -3,8 +3,13 @@ import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
     ${reset}
+
+    * {
+        box-sizing : border-box;
+    }
     
     body {
+        font-family: 'Inter', sans-serif;
         background-color: #f3f5ff;
     }
 `;
