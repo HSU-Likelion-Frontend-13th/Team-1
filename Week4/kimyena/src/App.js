3d12efa0329046635426f95b718e86268f44e8b8
yena <2371429@hansung.ac.kr>
2025-04-13 23:49:54 +0900

    feat: App.js 기본 코드 추가

--- Week4/kimyena/src/App.js
+++ Week4/kimyena/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import GlobalStyle from "./styles/globalStyles";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Hobby from "./components/Hobby";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+
       </header>
+      <nav>
+
+      </nav>
+      <section>
+
+      </section>
+      <section>
+        
+      </section>
     </div>
-  );
+  )
 }
 
 export default App;
