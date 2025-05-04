91e228b6a2f3d9b2e717deb04a46426de1ae41b7
yena <2371429@hansung.ac.kr>
2025-05-05 01:00:36 +0900

    feat: Header 컴포넌트 및 스타일 추가

--- Week7/yena/src/App.js
+++ Week7/yena/src/App.js
@@ -1,9 +1,11 @@
+import Header from "./components/header/Header";
 import { GlobalStyle } from "./styles/globalStyles";
 
 function App() {
   return (
     <div className = "App">
       <GlobalStyle />
+      <Header />
     </div>
   )
 }
