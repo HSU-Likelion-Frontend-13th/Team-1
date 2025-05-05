86ecb19d98fea4c3033f84be9fbb03a58ff8cf9a
yena <2371429@hansung.ac.kr>
2025-05-05 22:03:06 +0900

    feat: CommentEditor 컴포넌트 추가

---
+++ Week7/yena/src/components/main/comment/CommentEditor.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import * as S from "../Main.style.jsx";
+import Input from "../Input";
+import Button from "../Button";
+
+export default function CommentEditor({ inputText, setInputText, addComment }) {
+    return (
+        <S.AddCommentWrapper>
+            <Input
+                inputText = { inputText }
+                setInputText = { setInputText }
+            />
+            <Button text = "댓글 작성하기" addComment = { addComment }></Button>
+        </S.AddCommentWrapper>
+    )
+}
