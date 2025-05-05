3780ef3b771059cda7c9566b9d1b5b28a9a9f3e8
yena <2371429@hansung.ac.kr>
2025-05-05 21:32:07 +0900

    feat: Input 스타일 추가

--- Week7/yena/src/components/main/Main.style.jsx
+++ Week7/yena/src/components/main/Main.style.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+/* comment */
 export const SectionTitle = styled.div`
     font-size: larger;
     font-weight: 700;
@@ -16,3 +17,16 @@ export const CommentWrapper = styled.div`
       gap: 10px;
       margin: 1rem 0;
 `
+
+/* input */
+export const InputLayout = styled.div`
+  width: 100%;
+  height: 3rem;
+  border: 1px solid gray;
+  margin: 0.5rem 0;
+`;
+
+export const InputContainer = styled.input`
+  width: 100%;
+  height: 100%;
+`;
