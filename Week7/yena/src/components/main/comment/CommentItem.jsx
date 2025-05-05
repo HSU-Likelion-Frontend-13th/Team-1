4f7112e7e75b18d790c33b4dd7cb7638a41da065
yena <2371429@hansung.ac.kr>
2025-05-05 23:05:53 +0900

    feat: CommentItem 삭제 버튼 추가

--- Week7/yena/src/components/main/comment/CommentItem.jsx
+++ Week7/yena/src/components/main/comment/CommentItem.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import * as S from "../Main.style";
 
-export default function CommentItem({ commentText, comment, setComment}) {    return (
+export default function CommentItem({ commentText, comment, setComment}) {
+    const deleteComment = () => {
+        setComment(comment.filter((comm) => comm !== commentText));
+    };
+
+    return (
         <S.CommentItemLayout>
             <S.CommentText>{ commentText }</S.CommentText>
+            <S.DeleteButton onClick = { deleteComment }>삭제</S.DeleteButton>
         </S.CommentItemLayout>
     )
 }            
