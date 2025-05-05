9a08dcc4d40cb198493a26847a094cd8483c3cc2
yena <2371429@hansung.ac.kr>
2025-05-05 21:20:05 +0900

    feat: Comment 스타일 추가

---
+++ Week7/yena/src/components/main/Main.style.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const SectionTitle = styled.div`
+    font-size: larger;
+    font-weight: 700;
+`
+
+export const CommentLayout = styled.div`
+    width: 60vw;
+`
+
+export const CommentWrapper = styled.div`
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+      margin: 1rem 0;
+`
