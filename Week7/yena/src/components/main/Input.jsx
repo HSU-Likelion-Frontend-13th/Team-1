5747ad7e9caf3d42ad7136e55ad94e68b362799f
yena <2371429@hansung.ac.kr>
2025-05-06 16:03:04 +0900

    feat: 입력창에 메시지 추가

--- Week7/yena/src/components/main/Input.jsx
+++ Week7/yena/src/components/main/Input.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import * as S from "./Main.style.jsx";
 
-export default function Input({ inputText, setInputText }) {
+export default function Input({ inputText, setInputText, placeholder }) {
     return (
         <S.InputLayout>
             <S.InputContainer
                 type = "text"
                 value = { inputText }
                 onChange = { (e) => setInputText(e.target.value) }
+                placeholder = { placeholder }
             ></S.InputContainer>
         </S.InputLayout>
     )
--- Week7/yena/src/components/main/comment/CommentEditor.jsx
+++ Week7/yena/src/components/main/comment/CommentEditor.jsx
@@ -9,6 +9,7 @@ export default function CommentEditor({ inputText, setInputText, addComment }) {
             <Input
                 inputText = { inputText }
                 setInputText = { setInputText }
+                placeholder = "댓글을 작성해 주세요."
             />
             <Button text = "댓글 작성하기" addComment = { addComment }></Button>
         </S.AddCommentWrapper>
